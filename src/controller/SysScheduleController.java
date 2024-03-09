93a38bfbe032fbf93ae5a97cd8c9850ad50f999f
Li Tianyu <719471785@qq.com>
2024-03-09 17:37:02 +0800

    add filter

--- src/controller/SysScheduleController.java
+++ src/controller/SysScheduleController.java
@@ -2,12 +2,10 @@ package controller;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
-import java.lang.reflect.Method;
 
 /**
  * @author lty
@@ -16,17 +14,5 @@ import java.lang.reflect.Method;
 public class SysScheduleController extends BasicController {
     protected void add(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         System.out.println("add");
-    }
-
-    protected void find(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
-    }
-
-    protected void update(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
-    }
-
-    protected void remove(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
     }
 }
---
+++ src/filter/LoginFilter.java
@@ -0,0 +1,30 @@
+package filter;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import pojo.SysUser;
+
+import java.io.IOException;
+
+/**
+ * @author lty
+ */
+@WebFilter(urlPatterns = {"/showSchedule.html", "/schedule/*"})
+public class LoginFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) servletRequest;
+        HttpServletResponse resp = (HttpServletResponse) servletResponse;
+        HttpSession session = req.getSession();
+        SysUser sysUser = (SysUser) session.getAttribute("sysUser");
+        if(sysUser == null) {
+            resp.sendRedirect("/login.html");
+        } else {
+            filterChain.doFilter(req, resp);
+        }
+    }
+}
